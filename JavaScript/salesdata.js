52119a4890ca7d78ceb675a15c73811275ad61d5
ssutar934 <ssutar934@gmail.com>
2022-08-17 14:07:25 +0900

    update index page

--- JavaScript/salesdata.js
+++ JavaScript/salesdata.js
@@ -2,7 +2,9 @@
 
 function get_order_details() {
     try {
+
         let result = [];
+
         $.ajax({
             type: "POST",
             url: "SalesHandler.asmx/GetOrderData",
@@ -16,9 +18,25 @@ function get_order_details() {
 
             },
             error: function (xhr, status, error) {
-                alert(error + status);
+                /*alert(error + status);*/
             }
         });
+
+
+
+        result = [
+            { OrderNo: '21581', Product: 'MacBook Pro', Quantity: '1', Status: 'Completed' },
+            { OrderNo: '21582', Product: 'iPhone', Quantity: '5', Status: 'Completed' },
+            { OrderNo: '21583', Product: 'Google Pixel 7', Quantity: '3', Status: 'Processing' },
+            { OrderNo: '21584', Product: 'iPad mini ', Quantity: '2', Status: 'Cancelled' },
+            { OrderNo: '21585', Product: 'Asus VivoBook', Quantity: '1', Status: 'Processing' },
+            { OrderNo: '21586', Product: 'Panasonic', Quantity: '3', Status: 'Cancelled' },
+            { OrderNo: '21587', Product: 'HP SPECTRE X360', Quantity: '2', Status: 'Processing' },
+            { OrderNo: '55321', Product: 'ASUS ROG ZEPHYRUS', Quantity: '1', Status: 'Processing' },
+            { OrderNo: '55322', Product: 'Lenova Yoga 7i', Quantity: '1', Status: 'Cancelled' },
+            { OrderNo: '22323', Product: 'Dell XPS', Quantity: '2', Status: 'Completed' }
+        ]
+
 
         return result;
     } catch (e) {
@@ -44,7 +62,7 @@ function detailedordertrack(record) {
             $('#arrivedate').html('25/08/2022');
         }
         else if (record.Status == 'Cancelled') {
-           
+
         }
 
     } catch (e) {
